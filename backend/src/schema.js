7989b87cd9ddf6b1d8c54307f0c38a44e96a59fa
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2024-11-05 20:31:27 +0100

    Updated `schema.js`, added some comments.

--- backend/src/schema.js
+++ backend/src/schema.js
@@ -6,15 +6,20 @@ import { mergeResolvers } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
 
+// Root resolvers
 import resolvers from "./schema/resolvers.js";
 
+// `__dirname` workaround for ES modules
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const typeDefs =readFileSync(path.join(__dirname, './schema/typeDefs.graphql'), {encoding: 'utf-8'});
+// Root type definitions
+const typeDefs = readFileSync(path.join(__dirname, './schema/typeDefs.graphql'), {encoding: 'utf-8'});
 
-const resolversArr = loadFilesSync(path.join(__dirname, './schema/**/*.resolvers'), { extensions: ['js']});
+// Import resolvers and type definitions from all domains
+const resolversArr = loadFilesSync(path.join(__dirname, './schema/**/*.resolvers'), { extensions: ['js']}); // Import
 const typeDefsArr = loadFilesSync( path.join(__dirname, './schema/**/*'), { extensions: ['graphql']});
 
+// Create a final schema object form merged root and domain-specific type definitions and their resolvers
 export default makeExecutableSchema({
 	typeDefs: mergeTypeDefs([typeDefs, typeDefsArr]),
 	resolvers: mergeResolvers([resolvers, resolversArr])

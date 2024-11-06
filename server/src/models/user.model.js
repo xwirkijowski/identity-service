2e039e3f8d50361b00d66416189b82837dc3ed6d
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2024-11-06 14:03:14 +0100

    Updated user model, schema and resolvers. Added comments.

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -9,13 +9,35 @@ export default mongoose.model(
 		},
 		email: {
 			type: String,
-			required: true,
+			required: () => {
+				return (this.userType === 'NORMAL');
+			},
 			unique: true
 		},
 		password: {
 			type: String,
+			required: () => {
+				return (this.userType === 'NORMAL');
+			}
+		},
+		createdAt: {
+			type: String,
 			required: true
 		},
+		createdBy: {
+			type: mongoose.ObjectId,
+			required: true
+		},
+		updatedAt: {
+			type: String,
+		},
+		updatedBy: {
+			type: mongoose.ObjectId
+		},
+		version: {
+			type: Number,
+			default: 0
+		}
 	}, {
 		versionKey: false
 	})
--- server/src/schema/user/user.resolvers.js
+++ server/src/schema/user/user.resolvers.js
@@ -1,8 +1,37 @@
 export default {
 	User: {
+		__resolveType: (obj) => {
+			switch (obj.userType) {
+				case 'NORMAL':
+					return 'NormalUser';
+				case 'API':
+					return 'APIUser';
+				default:
+					return null;
+			}
+		}
+	},
+	NormalUser: {
 		id: ({_id}) => {
 			return _id;
-		}
+		},
+		createdBy: async ({createdBy}, _, {dataSources: {user}}) => {
+			return (createdBy) ? await user.findOne({_id: createdBy}) : null;
+		},
+		updatedBy: async ({updatedBy}, _, {dataSources: {user}}) => {
+			return (updatedBy) ? await user.findOne({_id: updatedBy}) : null;
+		},
+	},
+	APIUser: {
+		id: ({_id}) => {
+			return _id;
+		},
+		createdBy: async ({createdBy}, _, {dataSources: {user}}) => {
+			return (createdBy) ? await user.findOne({_id: createdBy}) : null;
+		},
+		updatedBy: async ({updatedBy}, _, {dataSources: {user}}) => {
+			return (updatedBy) ? await user.findOne({_id: updatedBy}) : null;
+		},
 	},
 	Query: {
 		user: async (_, args, {dataSources: {user}}) => {

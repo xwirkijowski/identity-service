6a64ddc60cabeeeaa4897e8558efa3b0970f752c
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2024-11-06 10:14:00 +0100

    Fixed resolver loading issues in `schema.js`.

--- backend/src/schema.js
+++ backend/src/schema.js
@@ -1,9 +1,9 @@
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath, pathToFileURL } from "url";
 import { readFileSync } from 'fs';
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { mergeResolvers } from "@graphql-tools/merge";
-import { loadFilesSync } from "@graphql-tools/load-files";
+import { loadFilesSync, loadFiles } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
 
 // Root resolvers
@@ -15,12 +15,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Root type definitions
 const typeDefs = readFileSync(path.join(__dirname, './schema/typeDefs.graphql'), {encoding: 'utf-8'});
 
-// Import resolvers and type definitions from all domains
-const resolversArr = loadFilesSync(path.join(__dirname, './schema/**/*.resolvers'), { extensions: ['js']}); // Import
-const typeDefsArr = loadFilesSync( path.join(__dirname, './schema/**/*'), { extensions: ['graphql']});
+// Import type definitions and resolvers from all domains
+const typeDefsArr = loadFilesSync(path.join(__dirname, './schema/**/*'), { extensions: ['graphql']});
+const resolversArr = await loadFiles(path.join(__dirname, './schema/**/*.resolvers.js'), {
+	requireMethod: async (p) => { return await import(pathToFileURL(p)); }
+});
 
 // Create a final schema object form merged root and domain-specific type definitions and their resolvers
 export default makeExecutableSchema({
-	typeDefs: mergeTypeDefs([typeDefs, typeDefsArr]),
-	resolvers: mergeResolvers([resolvers, resolversArr])
+	typeDefs: mergeTypeDefs([typeDefs, ...typeDefsArr]),
+	resolvers: mergeResolvers([resolvers, ...resolversArr])
 }, );
